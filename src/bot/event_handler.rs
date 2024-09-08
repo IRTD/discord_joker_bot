cfcae60f544ea6d1c8f3fe8418e270150d059f03
IRTD <reisbaellchen17@gmail.com>
2024-09-08 09:50:43 +0200

    Refreshes public IP every 2 hours now

--- src/bot/event_handler.rs
+++ src/bot/event_handler.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+use std::{process::Command, time::Instant};
+use tokio::sync::Mutex;
 
 use crate::quote::QuoteStack;
 
@@ -8,9 +9,12 @@ use serenity::{
     prelude::{Context, EventHandler},
 };
 
+const REFRESH_HOURS: usize = 2;
+
 pub struct Handler {
     stack_ref: QuoteStack,
-    mcip: String,
+    mcip: Mutex<String>,
+    refresh_time: Mutex<Instant>,
 }
 
 #[serenity::async_trait]
@@ -19,9 +23,15 @@ impl EventHandler for Handler {
         log::warn!("Read new message: {}", msg.content);
         match msg.content.as_str() {
             "!mcip" => {
+                let mut refresh_time = self.refresh_time.lock().await;
+                let mut mcip = self.mcip.lock().await;
+                if refresh_time.elapsed().as_secs() >= ( 60 ^ 2 * REFRESH_HOURS ) as u64  {
+                    *mcip = fetch_public_ip().unwrap();
+                    *refresh_time = Instant::now();
+                }
                 match msg
                     .channel_id
-                    .say(&ctx, self.mcip.clone())
+                    .say(&ctx, mcip.clone())
                     .await
                 {
                     Ok(_) => log::warn!("Responded"),
@@ -49,12 +59,15 @@ impl EventHandler for Handler {
 
 impl Handler {
     pub fn new(stack_ref: QuoteStack) -> Handler {
-        let mcip_bytes = Command::new("curl")
-            .args(["-4", "ifconfig.me"])
-            .output()
-            .unwrap()
-            .stdout;
-        let mcip = String::from_utf8(mcip_bytes).unwrap();
-        Handler { stack_ref, mcip }
+        let mcip = fetch_public_ip().unwrap().into();
+        Handler { stack_ref, mcip, refresh_time: Instant::now().into() }
     }
+}
+
+fn fetch_public_ip() -> anyhow::Result<String> {
+    let mcip_bytes = Command::new("curl")
+        .args(["-4", "ifconfig.me"])
+        .output()?
+        .stdout;
+    Ok(String::from_utf8(mcip_bytes)?)
 }
